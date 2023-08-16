e3b723d128416ddd9d07ed5e52acf5c50030e5c5
ian-mcquade <ijwmcquade@gmail.com>
2023-08-16 16:55:08 -0600

    Add contact upload route

--- oauth_app.py
+++ oauth_app.py
@@ -39,7 +39,10 @@ def callback():
     print('Scope:',scope)
     session['access_token'] = token_data['access_token']
     session['refresh_token'] = token_data['refresh_token']
-    return  f'<a href="{url_for("account_upload_csv")}">Account CSV Upload</a>'
+    return  (f'<a href="{url_for("account_upload_csv")}">Account CSV Upload</a></br>'
+             f'<a href="{url_for("contact_upload_csv")}">Contact CSV Upload </a>')
+            
+            
             #f"Access Token: {token_data['access_token']}<br>"
             #f"Refresh Token: {token_data['refresh_token']}<br>"
             
@@ -79,7 +82,7 @@ def api_request(url, payload):
         raise Exception("No access token in session. Please authorize first.")
     headers = {
         "Authorization": f"Bearer {session['access_token']}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     response = requests.post(url, json=payload, headers=headers)
 
@@ -190,6 +193,76 @@ def account_upload_csv():
         <input type="submit" value="Upload and Process">
     </form>
     '''
+    
+@app.route('/contact_upload_csv', methods=['GET', 'POST'])
+def contact_upload_csv():
+    sf_ids = [] #list of Salesforce id's that will be returned that were succesfully updated
+    new_line = '\n' # new line variable so that I can use in F strings
+    errors = []
+    
+    if request.method == 'POST':
+        csv_file = request.files['file']
+        if not csv_file.filename.endswith('.csv'):
+            return "Please upload a CSV file.", 400
+
+        csv_content = csv_file.read().decode('utf-8')
+        csv_data = csv.DictReader(io.StringIO(csv_content))
+        
+        successful_upserts = 0  # Counter for successful upserts
+        request_count = 0  # Counter to track API request counts
+        
+        for row in csv_data:
+            person_id = row['id']
+            crm_id = row['crm_id']
+            
+            # Define the URL and payload for the upsert request
+            url = f"https://api.salesloft.com/v2/person_upserts"
+            payload = {
+                "upsert_key": "id",
+                "id": person_id,
+                "crm_id": crm_id,
+                "crm_id_type": "salesforce"
+            }
+
+            # Make the API request
+            print(f"Upserting: ID {person_id} - CRM ID {crm_id}")
+
+            
+            response = api_request(url, payload)
+            #response_data = response.json()
+            if response.status_code // 100 == 2: #check if response code is the 200's
+                successful_upserts += 1  #increment upsert counter
+                sf_ids.append(crm_id) # add successful account id's to list 
+                
+
+            else:
+                error_message = f"Failed update for ID {person_id}. Response: {response.text}"
+                print(error_message)
+                errors.append(error_message)
+            
+            request_count += 1
+            if request_count == 600:  # Check if we've hit the rate limit
+                time.sleep(60)  # Sleep for 60 seconds
+                request_count = 0  # Reset the counter
+        
+        response_message = []
+
+        if successful_upserts:
+            response_message.append(f"People updated successfully! Total: {successful_upserts}")
+            response_message.append(f"List of SFDC ids: {new_line} {new_line.join(map(str, sf_ids))}")
+
+        if errors:
+            response_message.append("Some contacts failed to upsert:")
+            response_message.extend(errors)
+
+        return f"<pre>{new_line.join(response_message)}</pre>", 200 if not errors else 400
+
+    return '''
+    <form action="/contact_upload_csv" method="post" enctype="multipart/form-data">
+        Upload your CSV: <input type="file" name="file"><br>
+        <input type="submit" value="Upload and Process">
+    </form>
+    '''    
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
